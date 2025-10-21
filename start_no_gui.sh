0c4453075ae60b1f32de9ec4136ff6495dfe6827
你çWPS852 <w936148008@foxmail.com>
2025-10-21 18:27:07 +0800

    [wangpeishuo] bug: fix bugs of start_no_gui.sh

--- start_no_gui.sh
+++ start_no_gui.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "[INFO] 启动服务器..."
-gnome-terminal -- bash -c "python3 -m elevator_saga.server.simulator; exec bash"
+bash -c "python3 -m elevator_saga.server.simulator; exec bash"
 
 sleep 2
 echo "[INFO] 启动算法客户端..."
-gnome-terminal -- bash -c "python3 -m elevator_saga.client_examples.our_example; exec bash"
+bash -c "python3 -m elevator_saga.client_examples.our_example; exec bash"
 
 echo "[INFO] 所有进程已启动。"
